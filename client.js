63c8e70075dc45e7e54f17b64565da7784516aa2
Andre <andre@tos.world>
2024-08-12 19:23:25 -0300

    docker swoole

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 const env = typeof process !== 'undefined' ? process.env : (typeof Deno !== 'undefined' ? Deno.env.toObject() : {});
 
 const SERVERS = [  
-  { name: "Java", url: "ws://0.0.0.0:3012" },
+  { name: "PHP / Swoole", url: "ws://0.0.0.0:3013" },
   /*{ name: "Node", url: "ws://0.0.0.0:3001" },
   { name: "uWebsocket.js", url: "ws://0.0.0.0:3002" },  
   { name: "Bun", url: "ws://0.0.0.0:3004" },
@@ -12,11 +12,9 @@ const SERVERS = [
   { name: "C++ (Crow + TBB)", url: "ws://0.0.0.0:3010" },
   { name: "Rust", url: "ws://0.0.0.0:3011" },
   { name: "Java", url: "ws://0.0.0.0:3012" },
-  { name: "PHP / Swoole", url: "ws://0.0.0.0:3013" },
-  { name: "Go", url: "ws://0.0.0.0:3005" },*/
+  { name: "PHP / Swoole", url: "ws://0.0.0.0:3013" },*/
   //{ name: "Node (Worker Thread)", url: "ws://0.0.0.0:3014" },
   //{ name: "Zig", url: "ws://0.0.0.0:3008" }, // In implementation
-  //{ name: "Phyton3", url: "ws://0.0.0.0:3007/ws" },  
 ];
 
 //{ name: "Deno", url: "ws://0.0.0.0:3003" },
